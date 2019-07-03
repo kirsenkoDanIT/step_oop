f3f1711db7b0a7b224d98a662adaa9d9298c7a60
kirsenko <kirsenko8000@gmail.com>
2019-07-04 02:54:38 +0300

    step

---
+++ step/js/script.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const openFormBtn = document.querySelector('.page-header .btn')
+const closeBtn = document.querySelector('.form .close-btn')
+const form = document.querySelector('.form')
+const createNewVisitBtn = document.querySelector('.form .btn')
+const select = document.querySelector('#doctor-select')
+const inputs = document.querySelectorAll('.form p')
+form.style.display = 'none'
+
+openFormBtn.addEventListener('click', (e) => {
+    form.style.display === 'none' ?
+        form.style.display = '' :
+        form.style.display = 'none'
+    inputs.forEach(item => {
+        if (!item.dataset.name || item.dataset.name === select.options[select.selectedIndex].dataset.name) {
+            item.style.display = 'block'
+        } else
+            item.style.display = 'none'
+    })
+})
+
+closeBtn.addEventListener('click', (e) => {
+    form.style.display = 'none'
+})
+
+createNewVisitBtn.addEventListener('click', (event) => {
+    event.preventDefault
+})
+
+select.addEventListener('change', () => {
+    console.log(select.options[select.selectedIndex].dataset.name)
+
+    inputs.forEach(item => {
+        if (!item.dataset.name || item.dataset.name === select.options[select.selectedIndex].dataset.name) {
+            item.style.display = 'block'
+        } else item.style.display = 'none'
+    })
+})
+
+// inputs.forEach(item => {
+//     if (!item.dataset.name || item.dataset.name === select.options[select.selectedIndex].dataset.name) {
+//         item.style.display = 'block'
+//     } else item.style.display = 'none'
+// })

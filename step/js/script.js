4d8fdddc851a6dc132c856dabc34cad32eccee04
Vasiliy Tsyvinskyi <sodiicc@gmail.com>
2019-07-04 18:42:20 +0300

    Add Dantists

--- step/js/script.js
+++ step/js/script.js
@@ -22,16 +22,16 @@ class Visit {
         // this._visitDate = new Date(visitDate)
         this._visitDate = visitDate
         this._visitReason = visitReason
-
+        
         this._card = document.createElement('article')
         this._card.className = 'visit-card'
         let cardContent = `
         <p>${this._fullName}</p>
         <p>${this._visitDate}</p>
         <p>${this._visitReason}</p>`
-
+        
         this._card.innerHTML = cardContent
-
+        
         mainContent.appendChild(this._card)
     }
 
@@ -42,20 +42,59 @@ class Visit {
 
 class TherapistVisit extends Visit {
     constructor() {
-
+        
     }
 }
 
 class CardiologistVisit extends Visit {
     constructor() {
-
+        
     }
 }
 
 class DentistVisit extends Visit {
-    constructor() {
+    constructor(fullName, inputDate, lastVisit, reasonVisit, textArea){
+        super()
+        this._fullName = fullName
+        let cardContent = `
+        <p>${this._fullName}</p>
+        <p>Дантист</p>        
+        `
+        this._card.innerHTML = cardContent
+        this._card.style.backgroundColor = 'yellow'
+
+        let showMore = document.createElement('article')
+        showMore.innerText = 'Показати більше'
+        this._card.appendChild(showMore)
+
+        let inputs = [
+        this._date = document.createElement('article'),
+        this._lastVisit = document.createElement('article'),
+        this._reason = document.createElement('article'),
+        this._textArea = document.createElement('article')
+        ]
+        
+        this._date.innerText = inputDate
+        this._lastVisit.innerText = lastVisit
+        this._reason.innerText = reasonVisit
+        this._textArea.innerText = textArea
+        
+        inputs.forEach((el)=>{
+            this._card.appendChild(el)
+            el.style.display= 'none'
+        })
+        
+        showMore.style.cursor = 'pointer'
+
+        showMore.addEventListener('click', ()=>{
+            showMore.style.display = 'none'
+            inputs.forEach((el)=>{
+                el.style.display= ''
+            })
+        })
 
     }
+    
 }
 
 function addVisit() {
@@ -80,10 +119,14 @@ createNewVisitBtn.addEventListener('click', (e) => {
 
     let inputName = document.querySelector('#name').value
     let inputDate = document.querySelector('#date').value
+    let lastVisit = document.querySelector('#last-visit').value
+    let reasonVisit = document.querySelector('#reason').value
+    let textArea = document.querySelector('#text-area').value
     let doctorsName = select.options[select.selectedIndex].value
+    console.log('doctorsName', doctorsName)
     // let inputReason = document.querySelector('#reason').value
 
-    new Visit(inputName, inputDate, doctorsName)
+    new DentistVisit(inputName, inputDate, lastVisit, reasonVisit, textArea)
 
     form.style.display = 'none'
     // input.forEach(item => {

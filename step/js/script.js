921ed11b9cafda11ce0793c533b21be8a534d0bb
kirsenko <kirsenko8000@gmail.com>
2019-07-04 04:39:49 +0300

    step

--- step/js/script.js
+++ step/js/script.js
@@ -1,5 +1,11 @@
 'use strict'
 
+
+
+const fullName = document.querySelector('#name')
+
+console.log(fullName.value);
+
 const openFormBtn = document.querySelector('.page-header .btn')
 const closeBtn = document.querySelector('.form .close-btn')
 const form = document.querySelector('.form')
@@ -7,6 +13,39 @@ const createNewVisitBtn = document.querySelector('.form .btn')
 const select = document.querySelector('#doctor-select')
 const inputs = document.querySelectorAll('.form p')
 form.style.display = 'none'
+
+class Visit {
+    constructor(fullName, visitDate, visitReason) {
+        this._fullName = fullName
+        this._visitDate = new Date(visitDate)
+        this._visitReason = visitReason
+    }
+
+    removeVisit() {
+
+    }
+}
+
+class TherapistVisit extends Visit {
+    constructor() {
+
+    }
+}
+
+class CardiologistVisit extends Visit {
+    constructor() {
+
+    }
+}
+
+class DentistVisit extends Visit {
+    constructor() {
+
+    }
+}
+
+
+
 
 openFormBtn.addEventListener('click', (e) => {
     form.style.display === 'none' ?
